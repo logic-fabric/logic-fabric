dc7922a0c3de563f47a91aee2798432f6b7623fb
logic-fabric <lowic@logic-fabric.fr>
2022-11-22 05:52:45 +0100

    content: update Curriculum Vitae

--- js/data/content-data.js
+++ js/data/content-data.js
@@ -165,7 +165,7 @@ export const OTHER_PROJECTS = [
 
 export const CURRICULUM_VITAE = [
   {
-    title: "Développeur Front-end",
+    title: "Développeur Front-end | designOps",
     type: "job",
     organization: "Padam Mobility",
     period: "novembre 2021 - aujourd'hui",
